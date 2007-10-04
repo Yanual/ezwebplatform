0ee725b53c21d027600d219de972163668913b10
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-10-04 19:34:04 +0000

    Ver #33. Entorno Pruebas
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@39 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/js/common/uris.js
+++ ezweb/src/js/common/uris.js
@@ -1,6 +1,5 @@
-function URIConstants () {
-	URIConstants.prototype.IGADGETS = new Template('/user/#{user}/igadgets');	
-	URIConstants.prototype.IGADGETS_VARIABLES = new Template('/user/#{user}/igadgets/allVariables');
-}
+URIConstants.prototype.IGADGETS = new Template('/user/#{user}/igadgets');	
+URIConstants.prototype.IGADGETS_VARIABLES = new Template('/user/#{user}/igadgets/allVariables');
+
 
 
