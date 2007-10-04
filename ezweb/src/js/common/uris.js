5f7e0ca260a2ce59ca30874f294d0c1816070612
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-10-04 20:46:56 +0000

    Ver #32. PersitenceEngine
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@49 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/js/common/uris.js
+++ ezweb/src/js/common/uris.js
@@ -3,5 +3,4 @@ function URIConstants () {
 }
 
 URIConstants.prototype.IGADGETS = new Template('/user/#{user}/igadgets');	
-//URIConstants.prototype.IGADGETS_VARIABLES = new Template('/user/#{user}/igadgets/allVariables');
-URIConstants.prototype.IGADGETS_VARIABLES = 'ezWeb.html';
+URIConstants.prototype.IGADGETS_VARIABLES = new Template('/user/#{user}/igadgets/allVariables');
