9dad888d98de98b821f68f04450c1ed1f251a8d0
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-10-04 18:43:10 +0000

    Ver #32.
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@31 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

---
+++ ezweb/src/js/persistenceEngine/PersistenceEngine.js
@@ -0,0 +1,39 @@
+var PersitenceEngineFactory = function () {
+
+	// *********************************
+	// SINGLETON INSTANCE
+	// *********************************
+	var instance = null;
+
+	function PersitenceEngine () {
+		
+		// ****************
+		// PUBLIC METHODS 
+		// ****************
+		PersitenceEngine.prototype.send_get = function (url, context, successHandler, errorHandler) { 
+			new Ajax.Request(url, {
+				method: 'get',
+				onSuccess: context[callbackHandler],
+				onError: context[onError]
+			});
+		} 
+		
+		PersitenceEngine.prototype.send_post = function (url, value, context, successHandler, errorHandler) {}
+		PersitenceEngine.prototype.send_delete = function (url, context, successHandler, errorHandler){}
+		PersitenceEngine.prototype.send_update = function (url, value, context, successHandler, errorHandler){}
+	}
+	
+	// *********************************
+	// SINGLETON GET INSTANCE
+	// *********************************
+	return new function() {
+    	this.getInstance = function() {
+    		if (instance == null) {
+        		instance = new PersitenceEngine();
+            	instance.constructor = null;
+         	}
+         	return instance;
+       	}
+	}
+	
+}();
