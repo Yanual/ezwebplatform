8986a87d6399712c2a926f4863ec8aca6ffed5db
lmayllon <lmayllon@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-10-04 14:38:31 +0000

    Ver #17. Interface
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@24 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/js/lib/interface.js
+++ ezweb/src/js/lib/interface.js
@@ -97,10 +97,10 @@ var PersitenceEngineFactory = function () {
 		// ****************
 		// PUBLIC METHODS 
 		// ****************
-		PersitenceEngine.prototype.send_get = function (url) {} 
-		PersitenceEngine.prototype.send_post = function (url, value) {}
-		PersitenceEngine.prototype.send_delete = function (url){}
-		PersitenceEngine.prototype.send_update = function (url, value){}
+		PersitenceEngine.prototype.send_get = function (url, asynHandler) {} 
+		PersitenceEngine.prototype.send_post = function (url, value, asynHandler) {}
+		PersitenceEngine.prototype.send_delete = function (url, asynHandler){}
+		PersitenceEngine.prototype.send_update = function (url, value, asynHandler){}
 	}
 	
 	// *********************************
