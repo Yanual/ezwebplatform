2ecd76d0c480cfd8ee69daaba09341ca1b247052
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-10-15 10:46:51 +0000

    Ver #14. python persistencia
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@103 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/persistenceEngine/igadget/models.py
+++ ezweb/src/persistenceEngine/igadget/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Screen(models.Model):
+    uri = models.CharField(_('URI'), maxlength=500)
+
+    user = models.ForeignKey(User, verbose_name=_('User'))
+    
+    name = models.CharField(_('Name'), maxlength=30)
+
+    class Admin:
+        pass
+
+    def __unicode__(self):
+        return self.uri + " " + self.aspect
+
+class IGadget(models.Model):
+    uri = models.CharField(_('URI'), maxlength=500)
+        
+    gadget = models.ForeignKey(Gadget)
+    screen = models.ForeignKey(Screen)
+    
+    class Admin:
+        pass
+
+    def __unicode__(self):
+        return self.name
+
+
+
+class Variable(models.Model):
+    uri = models.CharField(_('URI'), maxlength=500)
+    
+    varDef = models.ForeignKey(VariableDef)
+    iGadget = models.ForeignKey(IGadget)
+    
+    # /////////////////////////////// jrosa, necesito que aqui se pueda guardar: una string, un integer, una fecha, etc.
+    # Me comentaste que algo se podía hacer :P A ver si resuelves ese jaleo
+    value = models.CharField(_('Value'), maxlength=30)
+
+    class Admin:
+        pass
+
+    def __unicode__(self):
+        return self.uri + " " + self.value
+            
