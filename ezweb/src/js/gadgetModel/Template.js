bb1401e7cfc6ab453b6484f05cb18db261409b89
jmostazo <jmostazo@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-10-15 14:38:28 +0000

    Ver #38. Cambios en el Template
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@105 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/js/gadgetModel/Template.js
+++ ezweb/src/js/gadgetModel/Template.js
@@ -28,14 +28,30 @@ function Template(template_) {
 			switch (rawVar.aspect) {
 				case Variable.prototype.PROPERTY:
 				case Variable.prototype.EVENT:
-					alert(rawVar.name);
 					objVars[rawVar.name] = new RWVariable(igadget_, rawVar.name, rawVar.aspect, rawVar.value);
 					break;
 				case Variable.prototype.SLOT:
 				case Variable.prototype.USER_PREF:
-					alert(rawVar.name);
 					objVars[rawVar.name] = new RVariable(igadget_, rawVar.name, rawVar.aspect, rawVar.value);
 					break;
+			}
+		}
+        return objVars;
+    }
+	
+	this.getUserPrefsId = function () {
+        
+		// JSON-coded Template-UserPrefs mapping	
+		// Constructing the structure 
+		 
+		var objVars = [];
+		var rawVars = variableList;
+		var rawVar = null;
+		for (i = 0; i<rawVars.length; i++) {
+			rawVar = rawVars[i];
+			if (rawVar.aspect == Variable.prototype.USER_PREF)
+			{
+					objVars.push(rawVar.name);
 			}
 		}
         return objVars;
