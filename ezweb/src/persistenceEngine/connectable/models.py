6c6f6dba8c069ed6c47154ee00ffee8cdd7f55bb
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-10-15 16:57:39 +0000

    Ver #14. python persistencia
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@111 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/persistenceEngine/connectable/models.py
+++ ezweb/src/persistenceEngine/connectable/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+from persistenceEngine.igadget.models import Variable
+     
+class In(models.Model):
+    uri = models.CharField(_('URI'), maxlength=500)
+    
+    name = models.CharField(maxlength=30)
+    variable = models.ForeignKey(Variable)
+    
+    class Meta:
+        unique_together = ('name', 'variable')
+
+    class Admin:
+        pass
+
+    def __unicode__(self):
+        return self.uri + " " + self.name
+    
+class Out(models.Model):
+    uri = models.CharField(_('URI'), maxlength=500)
+    
+    name = models.CharField(maxlength=30)
+    variable = models.ForeignKey(Variable)
+    
+    class Meta:
+        unique_together = ('name', 'variable')
+
+    class Admin:
+        pass
+
+    def __unicode__(self):
+        return self.uri + " " + self.name
+    
+class InOut(models.Model):
+    uri = models.CharField(_('URI'), maxlength=500)
+    
+    name = models.CharField(maxlength=30, primary_key=True)
+    
+    value = models.CharField(maxlength=100)
+    
+    inList = models.ManyToManyField(In)
+    outList = models.ManyToManyField(Out)
+        
+    class Admin:
+        pass
+
+    def __unicode__(self):
+        return self.uri + " " + self.name
+            
+
+
+
+            
