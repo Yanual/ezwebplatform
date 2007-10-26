59cd95f997ee2df6e936184fa5bbbc3b1a0defc1
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-10-26 13:51:54 +0000

    Ver #14. python persistencia
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@203 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

---
+++ ezweb/src/persistenceEngine/gadget/gadgetCodeParser.py
@@ -0,0 +1,44 @@
+from HTMLParser import HTMLParser
+from urllib import urlopen
+
+from models import *
+
+class GadgetCodeParser(HTMLParser):
+# HTML parsing
+
+    xHTML = None
+    user_id = ""
+
+    def parseUserEvents(self, codeURI, user_id, gadgetURI):
+        self.user_id = user_id
+
+        xhtml = urlopen(codeURI).read()
+        
+        self.xHTML = XHTML (uri=gadgetURI + "/xhtml", code=xhtml)
+        self.xHTML.save()
+
+        self.feed(xhtml)
+
+    def handle_starttag(self, tag, attrs):
+        handler = None
+        event = None
+        id = None
+
+        for (name, value) in attrs:
+            if (name == 'ezweb:handler'):
+                handler = value
+                continue
+
+            if (name == 'ezweb:event'):
+                event = value
+                continue
+
+            if (name == 'id'):
+                id = value
+
+        if (handler != None and event != None and id != None):
+            mapping = UserEventsInfo(event=event, handler=handler,html_element=id, xhtml=self.xHTML)
+            mapping.save()
+
+    def getXHTML (self):
+        return self.xHTML
