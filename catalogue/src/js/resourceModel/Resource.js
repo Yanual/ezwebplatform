1960b6ae67879d3f58730e12cc1bc376373f0cc9
jmostazo <jmostazo@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-10-30 08:36:02 +0000

    Ver #63.
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@222 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- catalogue/src/js/resourceModel/Resource.js
+++ catalogue/src/js/resourceModel/Resource.js
@@ -16,6 +16,8 @@ function Resource( id_, resourceXML_, urlTemplate_) {
 	this.getUriImage = function() { return state.getUriImage(); }
 	this.getUriTemplate = function() { return state.getUriTemplate(); }
 	this.getUriWiki = function() { return state.getUriWiki(); }
+	this.getTags = function() { return state.getTags(); }
+	this.setTags = function(tags_) { state.setTags(tags_); }
 	this.getTagger = function() { return tagger; }
 	
 	this.paint = function(){
@@ -25,20 +27,20 @@ function Resource( id_, resourceXML_, urlTemplate_) {
 									"<div class='top'></div>" +
 									"<div class='toolbar'>" +
 										"<div id='" + id + "_toolbar' style='display:none;'>" +
-											"<a href='#' onmouseover=\"UIUtils.changeImage('" + id_ + "_description', 'images/description.png');\" onmouseout=\"UIUtils.changeImage('" + id_ + "_description', 'images/description_gray.png');\">" +
-												"<img id='" + id + "_description' src='images/description_gray.png' alt=''></img>" +
+											"<a title='Ver Descripci&oacute;n' href='#' onmouseover=\"UIUtils.changeImage('" + id_ + "_description', 'images/description.png');\" onmouseout=\"UIUtils.changeImage('" + id_ + "_description', 'images/description_gray.png');\">" +
+												"<img id='" + id + "_description' src='images/description_gray.png' alt='Description'></img>" +
 											"</a>" +
-											"<a href='" + state.getUriWiki() + "' target='_blank'  onmouseover=\"UIUtils.changeImage('" + id_ + "_wiki', 'images/wiki.png');\" onmouseout=\"UIUtils.changeImage('" + id_ + "_wiki', 'images/wiki_gray.png');\">" +
+											"<a title='Acceder a la Wiki' href='" + state.getUriWiki() + "' target='_blank'  onmouseover=\"UIUtils.changeImage('" + id_ + "_wiki', 'images/wiki.png');\" onmouseout=\"UIUtils.changeImage('" + id_ + "_wiki', 'images/wiki_gray.png');\">" +
 												"<img id='" + id + "_wiki' src='images/wiki_gray.png' alt='Wiki'></img>" +
 											"</a>" +
-											"<a href='" + state.getUriTemplate() + "' target='_blank' onmouseover=\"UIUtils.changeImage('" + id_ + "_template', 'images/template.png');\" onmouseout=\"UIUtils.changeImage('" + id_ + "_template', 'images/template_gray.png');\">" +
+											"<a title='Ver el Template' href='" + state.getUriTemplate() + "' target='_blank' onmouseover=\"UIUtils.changeImage('" + id_ + "_template', 'images/template.png');\" onmouseout=\"UIUtils.changeImage('" + id_ + "_template', 'images/template_gray.png');\">" +
 												"<img id='" + id + "_template' src='images/template_gray.png' alt='Template'></img>" +
 											"</a>" +
 										"</div>" +
 									"</div>" +
 									"<div id='" + id + "_content' class='content'>" +
 										"<div class='title'>" + state.getName() + "</div>" +
-										"<div class='image'><a href='javascript:UIUtils.showResourceInfo(\"" + id + "\");'><img src='" + state.getUriImage() + "' alt='Click para m&aacute;s informaci&oacute;n'></img></a></div>" +
+										"<div class='image'><a title='Mostrar informaci&oacute;n del recurso' href='javascript:UIUtils.showResourceInfo(\"" + id + "\");'><img src='" + state.getUriImage() + "' alt='Mostrar informaci&oacute;n del recurso'></img></a></div>" +
 										"<button onclick='CatalogueFactory.getInstance().addResourceToShowCase(\"" + id + "\");'>A&ntilde;adir a la Paleta</button>" +
 									"</div>" +
 									"<div id='" + id + "_bottom' class = 'bottom'></div>" +
@@ -71,10 +73,6 @@ function Resource( id_, resourceXML_, urlTemplate_) {
 									"<div class='link'><a href='" + state.getUriTemplate() + "' target='_blank'>Acceder al Template</a></div>" +
 								"</div>" +
 								"<button onclick='CatalogueFactory.getInstance().addResourceToShowCase(UIUtils.getSelectedResource());'>A&ntilde;adir a la Paleta</button>";
-	}
-	
-	this.addTag = function(value_) {
-		var tableInfo = document.getElementById("info_resource");
 	}
 	
 	// *******************
@@ -139,6 +137,7 @@ function ResourceState(resourceXML_) {
 	var uriImage = null;
 	var uriWiki = null;
 	var uriTemplate = null;
+	var tags = [];
 	
 	// Parsing XML Resource
 	// Constructing the structure
@@ -162,4 +161,7 @@ function ResourceState(resourceXML_) {
 	this.getUriImage = function() { return uriImage; }
 	this.getUriTemplate = function() { return uriTemplate; }
 	this.getUriWiki = function() { return uriWiki; }
+
+	this.setTags = function(tags_) { tags = tags_; }
+	this.getTags = function() { return tags; }
 }
