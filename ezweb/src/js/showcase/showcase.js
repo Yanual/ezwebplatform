0b4cd49c5f13dab74f895cfff65df225a8b5e381
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-02 16:35:55 +0000

    Ver #18.
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@281 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/js/showcase/showcase.js
+++ ezweb/src/js/showcase/showcase.js
@@ -155,7 +155,7 @@ var ShowcaseFactory = function () {
 			buffer.append('<center><img src="');
 			buffer.append(gadget.getImage()); 
 			buffer.append('" alt="Imagen cannot be shown" /><br/>\n');
-			buffer.append('<a href="javascript:opManager.addInstance(\'' + gadgetId_ + '\');		$(\"showcase_container\").style.display=\"block\"">add</a></center>\n');
+			buffer.append('<a href="javascript:opManager.addInstance(\'' + gadgetId_ + '\')">add</a></center>\n');
 			buffer.append('</div>\n');
 			
 			// Close Gadget Layer
