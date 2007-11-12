304559494ebad570e926fe9e2c756d24ae86bb0d
franh <franh@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-12 15:35:06 +0000

    Ver #49. Cambios correspondientes a la parte de catálogo
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@371 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/persistenceEngine/resource/utils.py
+++ ezweb/src/persistenceEngine/resource/utils.py
@@ -8,12 +8,12 @@ from persistenceEngine.resource.models import gadgetResource
 def get_xml_description(gadgetlist):
 
     xml_resource = ''
-    xml_tag=''
+    xml_tag = ''
     for e in gadgetlist:
 
         xml_tag = get_tags_by_resource(e.id)
 	  				
-	xml_resource +='<Resource>\n\
+	xml_resource += '<Resource>\n\
         <Vendor>'+e.vendor+'</Vendor>\n\
         <Name>'+e.short_name+'</Name>\n\
 	<Version>'+e.version+'</Version>\n\
