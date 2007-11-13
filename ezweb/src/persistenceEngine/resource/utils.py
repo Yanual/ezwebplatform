7c69a5db84bcc61a13df30c730508790f55bd1b4
franh <franh@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-13 10:59:42 +0000

    Ver #49. Cambios correspondientes a la parte de catálogo
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@385 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/persistenceEngine/resource/utils.py
+++ ezweb/src/persistenceEngine/resource/utils.py
@@ -1,28 +1,30 @@
 from xml.sax import saxutils
 from xml.sax import make_parser
 
-from persistenceEngine.tag.models import userTag
-from persistenceEngine.resource.models import gadgetResource
-from persistenceEngine.tag.utils import get_tags_by_resource
+from catalogue.tag.models import userTag
+from catalogue.resource.models import gadgetResource
+from catalogue.tag.utils import get_tags_by_resource
 
 def get_xml_description(gadgetlist):
 
     xml_resource = ''
     xml_tag=''
     for e in gadgetlist:
-			#xml_tag = get_tags_by_resource(e.id)
+
+        xml_tag = get_tags_by_resource(e.id)
 	  				
-			xml_resource +='<Resource>\n\
-      <Vendor>'+e.vendor+'</Vendor>\n\
-      <Name>'+e.short_name+'</Name>\n\
-			<Version>'+e.version+'</Version>\n\
-			<Author>'+e.author+'</Author>\n\
-			<Mail>'+e.mail+'</Mail>\n\
+	xml_resource +='<Resource>\n\
+        <Vendor>'+e.vendor+'</Vendor>\n\
+        <Name>'+e.short_name+'</Name>\n\
+	<Version>'+e.version+'</Version>\n\
+	<Author>'+e.author+'</Author>\n\
+	<Mail>'+e.mail+'</Mail>\n\
     	<Description>'+e.description+'</Description>\n\
     	<ImageURI>'+e.image_uri+'</ImageURI>\n\
     	<WikiURI>'+e.wiki_page_uri+'</WikiURI>\n\
-			<TemplateURI>'+e.template_uri+'</TemplateURI>\n\
-   		</Resource>'
+	<TemplateURI>'+e.template_uri+'</TemplateURI>\n\
+	'+xml_tag+'\n\
+   	</Resource>'
 		
     response = xml_resource
     return response
