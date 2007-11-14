e70150b4bd5ee243f535d928d98c7d54a3f551b9
franh <franh@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-14 07:01:06 +0000

    Ver #49. Cambios correspondientes a la parte de catálogo
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@440 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/persistenceEngine/resource/views.py
+++ ezweb/src/persistenceEngine/resource/views.py
@@ -26,11 +26,11 @@ class GadgetsCollection(Resource):
 	# Tell the parser to use our handler
 	parser.setContentHandler(handler)
 	
-	#template_uri = request.__getitem__('template_uri')
+	template_uri = request.__getitem__('template_uri')
 		
 	# Parse the input
-	#parser.parse(template_uri)
-	parser.parse("http://europa.ls.fi.upm.es/~mac/template.xml")
+	parser.parse(template_uri)
+	#parser.parse("http://europa.ls.fi.upm.es/~mac/template.xml")
 			
 	gadget=gadgetResource()
 
@@ -43,7 +43,7 @@ class GadgetsCollection(Resource):
 	gadget.mail=handler._mail
 	gadget.image_uri=handler._imageURI
 	gadget.wiki_page_uri=handler._wikiURI
-	#gadget.template_uri=''
+	gadget.template_uri=template_uri
 	gadget.creation_date=datetime.today()
 	
 	
