9b97b2ef2901fe9243f063ae0bf21fd6bd137799
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-19 14:37:52 +0000

    Ver #65.
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@551 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/persistenceEngine/commons/get_data.py
+++ ezweb/src/persistenceEngine/commons/get_data.py
@@ -62,7 +62,8 @@ def get_igadget_data(data):
     data_fields = data['fields']
     gadget = Gadget.objects.get(pk=data_fields['gadget'])
     position = Position.objects.get(pk=data_fields['position'])
-      
+
+    data_ret['id'] = data_fields['uri']
     data_ret['id'] = data['pk']
     data_ret['uri'] = data_fields['uri']
     data_ret['gadget'] = gadget.uri
