f26f8f258809ac9c29b959131b2799b603e9f93e
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-19 14:38:08 +0000

    Ver #65.
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@552 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/persistenceEngine/urls.py
+++ ezweb/src/persistenceEngine/urls.py
@@ -4,11 +4,16 @@ from django.conf.urls.defaults import *
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django_restapi.resource import Resource
+from persistenceEngine.resource.views import addToPlatform
+
 
 urlpatterns = patterns('',
     # Static content
     # (r'^site_media/(.*)$', 'ezweb.views.static.serve', {'document_root': path.expanduser('~/ezweb/media')}),
     (r'^ezweb/(.*)$', 'django.views.static.serve', {'document_root': path.join('opt', 'ezweb', 'src', 'js')}),
+
+    (r'^ezweb_catalogue/(.*)$', 'django.views.static.serve', {'document_root': path.join('opt', 'catalogue', 'src')}),
+
 
     # EzWeb
     (r'^$', include('trial.urls')),
@@ -24,10 +29,11 @@ urlpatterns = patterns('',
     (r'^user/(?P<user_name>[-ÑñáéíóúÁÉÍÓÚ\w]+)/connectable(s)?/', include('connectable.urls')),
     
     # Catalogue Resource
-    (r'^user/(?P<user_name>\w+)/catalogue/resource(s)?/',include('resource.urls')),
+    (r'^user/(?P<user_name>[-ÑñáéíóúÁÉÍÓÚ\w]+)/catalogue/resource(s)?/',include('resource.urls')),
+    (r'^user/(?P<user_name>[-ÑñáéíóúÁÉÍÓÚ\w]+)/platform/$', addToPlatform),
 
     # Catalogue Tag
-    (r'^user/(?P<user_name>\w+)/catalogue/tag(s)?/',include('tag.urls')),
+    (r'^user/(?P<user_name>[-ÑñáéíóúÁÉÍÓÚ\w]+)/catalogue/tag(s)?/',include('tag.urls')),
     
    
     # Django contrib
