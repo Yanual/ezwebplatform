a682f5d829522b8986a6c1e4919ee48c4027c78f
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-19 16:11:22 +0000

    Ver #65. Saving resource model data
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@561 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/persistenceEngine/save_fixtures.sh
+++ ezweb/src/persistenceEngine/save_fixtures.sh
@@ -3,5 +3,7 @@
 ./manage.py dumpdata --format=xml --indent=4 connectable > connectable/fixtures/initial_data.xml
 ./manage.py dumpdata --format=xml --indent=4 gadget > gadget/fixtures/initial_data.xml
 ./manage.py dumpdata --format=xml --indent=4 igadget > igadget/fixtures/initial_data.xml
+./manage.py dumpdata --format=xml --indent=4 resource > resource/fixtures/initial_data.xml
+
 
 
