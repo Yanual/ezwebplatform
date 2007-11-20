a7cb3e4c0c0bfb8fc32ba220991df4bd6f5b210f
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-20 11:33:12 +0000

    Ver #65. Saving fixtures of the 5 models
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@568 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/persistenceEngine/save_fixtures.sh
+++ ezweb/src/persistenceEngine/save_fixtures.sh
@@ -4,6 +4,7 @@
 ./manage.py dumpdata --format=xml --indent=4 gadget > gadget/fixtures/initial_data.xml
 ./manage.py dumpdata --format=xml --indent=4 igadget > igadget/fixtures/initial_data.xml
 ./manage.py dumpdata --format=xml --indent=4 resource > resource/fixtures/initial_data.xml
+./manage.py dumpdata --format=xml --indent=4 tag > tag/fixtures/initial_data.xml
 
 
 
