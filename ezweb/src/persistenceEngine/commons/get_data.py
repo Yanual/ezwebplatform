4941c7020c9cbd6f0247a9642ee39fc5cfa02df1
jrosa <jrosa@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-27 11:31:56 +0000

    Ver #13.
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@584 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/persistenceEngine/commons/get_data.py
+++ ezweb/src/persistenceEngine/commons/get_data.py
@@ -6,39 +6,35 @@ from igadget.models import Variable, VariableDef, Position
 from connectable.models import In, Out
 
 def get_gadget_data(data):
-   data_ret = {}
-   data_fields = data['fields']
+    data_ret = {}
+    data_fields = data['fields']
 
-   data_template = get_object_or_404(Template, id=data_fields['template'])
-   data_variabledef = VariableDef.objects.filter(template=data_template.id).values('aspect',
-'name', 'type')
-   data_code = get_object_or_404(XHTML.objects.all().values('uri'),
-id=data_fields['xhtml'])
-   data_elements = UserEventsInfo.objects.filter(xhtml=data_fields['xhtml']).values('event',
-'handler', 'html_element')
+    data_code = get_object_or_404(XHTML.objects.all().values('uri'), id=data_fields['xhtml'])
+    data_elements = UserEventsInfo.objects.filter(xhtml=data_fields['xhtml']).values('event', 'handler', 'html_element')
 
-   data_ret['name'] = data_fields['name']
-   data_ret['vendor'] = data_fields['vendor']
-   data_ret['description'] = data_fields['description']
-   data_ret['tags'] = data_fields['tags']
-   data_ret['uri'] = data_fields['uri']
-   data_ret['wikiURI'] = data_fields['wikiURI']
-   data_ret['imageURI'] = data_fields['imageURI']
-   data_ret['version'] = data_fields['version']
-   data_ret['user'] = data_fields['user']
-   data_ret['mail'] = data_fields['mail']
-   data_ret['shared'] = data_fields['shared']
-   data_ret['last_update'] = data_fields['last_update']
-   data_ret['template'] = {}
-   data_ret['template']['size'] = {}
-   data_ret['template']['size']['width'] = data_template.width
-   data_ret['template']['size']['height'] = data_template.height
-   data_ret['template']['variables'] = data_variabledef
-   data_ret['image'] = data_template.image
-   data_ret['xhtml'] = data_code
-   data_ret['xhtml']['elements'] = data_elements
+    data_ret['name'] = data_fields['name']
+    data_ret['vendor'] = data_fields['vendor']
+    data_ret['description'] = data_fields['description']
+    data_ret['tags'] = data_fields['tags']
+    data_ret['uri'] = data_fields['uri']
+    data_ret['wikiURI'] = data_fields['wikiURI']
+    data_ret['imageURI'] = data_fields['imageURI']
+    data_ret['version'] = data_fields['version']
+    data_ret['user'] = data_fields['user']
+    data_ret['mail'] = data_fields['mail']
+    data_ret['shared'] = data_fields['shared']
+    data_ret['last_update'] = data_fields['last_update']
+    data_ret['template'] = {}
+    data_ret['template']['size'] = {}
+    data_ret['template']['size']['width'] = data_template.width
+    data_ret['template']['size']['height'] = data_template.height
+    data_ret['template']['variables'] = data_variabledef
+    data_ret['image'] = data_template.image
+    data_ret['xhtml'] = data_code
+    data_ret['xhtml']['elements'] = data_elements
 
-   return data_ret
+    return data_ret
+
 
 def get_inout_data(data):
     data_ret = {}
