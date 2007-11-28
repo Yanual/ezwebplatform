b45b937aece3613ed87028d08728e36c1806f29e
jrosa <jrosa@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-28 10:31:46 +0000

    Ver #68. Organizado definitivo del trunk del proyecto.
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@608 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/old/js/common/uris.js
+++
@@ -1,6 +0,0 @@
-function URIConstants () {
-
-}
-
-URIConstants.prototype.IGADGETS = new Template('/user/#{user}/igadgets');	
-URIConstants.prototype.IGADGETS_VARIABLES = new Template('/user/#{user}/igadgets/allVariables');
