a76bc3ffc6ce43c9a225fe1029f62633e76a492c
lmayllon <lmayllon@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-28 08:06:31 +0000

    Ver #14. Modelo de Persistencia de IGadget. Previo a la mezcla de rama PREALFA y HEAD
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@591 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/prealfa/src/persistenceEngine/igadget/views.py
+++ ezweb/prealfa/src/persistenceEngine/igadget/views.py
@@ -14,42 +14,57 @@ from commons.authentication import user_authentication
 from commons.get_data import get_igadget_data
 from commons.utils import json_encode
 
-from igadget.models import IGadget, Screen, Position
-from gadget.models import Gadget
+from gadget.models import Gadget, VariableDef
+from igadget.models import *
 
 @transaction.commit_on_success
 def SaveIGadget(igadget, user, screen_id, igadget_id):
     uri = igadget.get('uri')
-# 	id = uri.partition("/igadgets/")[2]
-    gadget_uri = igadget.get('gadget')#TODO add gadget id in JSON
+    if not igadget_id:
+        if uri.find('igadgets'):
+            igadget_id = uri.partition('/igadgets/')[2]
+        else:
+            igadget_id = uri.partition('/igadget/')[2]
+
+    gadget_uri = igadget.get('gadget')
     width = igadget.get('width')
     height = igadget.get('height')
     top = igadget.get('top')
     left = igadget.get('left')
-    if not uri or not gadget_uri or not width or not height or not top or not left:
+   
+    if not igadget_id or not uri or not gadget_uri or not width or not height or not top or not left:
         raise Exception('Malformed iGadget JSON')   
-            
-    position = Position (uri=uri + '/position', posX=left, posY=top, height=height, width=width)
-    position.save()
 
+    #Gets current user screen
     try:
         screen = Screen.objects.get(id=screen_id)
     except Screen.DoesNotExist: 
+        #TODO Screen would have to be created yet. Remove from last release 
         screen_uri = "/user/" + user.username + '/screen/' + str(screen_id)
-        #TODO screen name is not given by JSON and it is not null in database, where can we get it?
         screen = Screen (id=screen_id, uri=screen_uri, name='myScreen', user=user) 
         screen.save()            
-
+            
+    position = Position (uri=uri + '/position', posX=left, posY=top, height=height, width=width)
+    position.save()
+            
     try:
-        gadget = Gadget.objects.get(uri=gadget_uri)		
+        gadget = Gadget.objects.get(uri=gadget_uri)
+        new_igadget = IGadget (id=igadget_id ,uri=uri, gadget=gadget, screen=screen, position=position)
+        new_igadget.save()
+        variableDefs = VariableDef.objects.filter(template=gadget.template)
+        for varDef in variableDefs:
+            if not gadget.default_value:
+                var_value = gadget.default_value
+            else:
+                var_value = ''
+            var = Variable (uri=uri + '/variable/' + varDef.name, vardef=varDef, igadget=new_igadget, value=var_value)
+            var.save() 
     except Gadget.DoesNotExist:
         raise Gadget.DoesNotExist('iGadget without associated gadget')
-            
-	if igadget_id:
-	   new_igadget = IGadget (id=igadget_id ,uri=uri, gadget=gadget, screen=screen, position=position)
-	else:
-		new_igadget = IGadget (uri=uri, gadget=gadget, screen=screen, position=position)
-        new_igadget.save()
+    except VariableDef.DoesNotExist:
+        #iGadget has no variables
+        pass
+
 
 class IGadgetCollection(Resource):
     def read(self, request, user_name, screen_id=None):
@@ -119,7 +134,7 @@ class IGadgetEntry(Resource):
         else:
             igadget = IGadget.objects.filter(screen=screen_id)
             data = serializers.serialize('python', igadget, ensure_ascii=False)
-            igadget_data = get_igadget_data(data)
+            igadget_data = get_igadget_data(data[0])
         return HttpResponse(json_encode(igadget_data), mimetype='application/json; charset=UTF-8')
     
     def create(self, request, user_name, igadget_id, screen_id=None):
@@ -129,7 +144,7 @@ class IGadgetEntry(Resource):
         if not screen_id:
             screen_id = 1
 
-        if not request.has_key('igadgets'):
+        if not request.has_key('igadget'):
             return HttpResponseBadRequest('<error>iGadget JSON expected</error>')
 
         #TODO we can make this with deserializers (simplejson)
@@ -147,5 +162,27 @@ class IGadgetEntry(Resource):
             return HttpResponseServerError('<error>iGadget cannot be saved: %s</error>' % e)
 
 
+    def delete(self, request, user_name, igadget_id, screen_id=None):
+        user = user_authentication(user_name)
 
+        #TODO by default. Remove in final release
+        if not screen_id:
+            screen_id = 1
 
+        igadget = get_object_or_404(IGadget, user=user, id=igadget_id)
+        igadget.delete()
+        
+class GadgetVariable(Resource):
+    def read(self, request, user_name, igadget_id, var_name, screen_id=None):
+        user = user_authentication(user_name)
+        igadget = IGadget.objects.get(id=igadget_id, user=user)
+        data = serializers.serialize('python', igadget, ensure_ascii=False)
+        print data
+        #return HttpResponse(json_encode(data_fields), mimetype='application/json; charset=UTF-8')
+        return HttpResponse('')
+    
+    def update(self, request, user_name, igadget_id, var_name, screen_id=None):
+        user = user_authentication(user_name)
+        gadget = get_object_or_404(Gadget, user=user, vendor=vendor, name=name, version=version)
+        gadget.save()
+        return HttpResponse('')
