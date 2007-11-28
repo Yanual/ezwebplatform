7ccfd700e3eb507bac1248b071a52590edbfee3b
aarranz <aarranz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-28 16:57:38 +0000

    Ver #67. Patch from jlopez. Added support for redirecting http headers and cookies.
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@615 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/trunk/proxy/views.py
+++ ezweb/src/trunk/proxy/views.py
@@ -1,10 +1,10 @@
-from urllib import urlencode
 from urllib2 import *
 from django_restapi.resource import Resource
 from django_restapi.responder import *
+from proxy.utils import is_valid_header
 
-from django.http import Http404, HttpResponse, HttpResponseRedirect, HttpResponseServerError
-import sys
+from django.http import Http404, HttpResponse, HttpResponseServerError
+import string
 
 class Proxy(Resource):
 
@@ -25,18 +25,32 @@ class Proxy(Resource):
             params = request.POST['params']
  	
 	# HTTP call
-	if (method == "GET"):
-	    opener = build_opener()
-	    request = Request (url)
-	    try:
-		return HttpResponse(opener.open(request))
-	    except Exception, e:
-		return HttpResponseServerError("<error>%s</error>" % e, mimetype='text/xml; charset=UTF-8')
-	if (method== "POST"):
-	    opener = build_opener()
-	    request = Request (url,params)
-	    try:
-		return HttpResponse(opener.open(request))
-	    except Exception, e:
-		return HttpResponseServerError("<error>%s</error>" % e, mimetype='text/xml; charset=UTF-8')
-
+	try:
+	    # Request creation
+            req = Request (url)
+	    # Add POST parameters
+	    if (method=="POST"):
+		req.add_data(params)
+	    # Add original request Headers to the request
+	    for header in request.META.items():
+	   	req.add_header(header[0],header[1])
+	    # The same with cookies
+	    cookies = ''
+	    for cookie in request.COOKIES.items():
+		cookies = cookies + cookie[0] + '=' + cookie[1] + '; '	
+	    req.add_header('Cookie', cookies)
+	    # Open the request
+	    file = urlopen(req)
+	    # Add content-type header to the response
+	    if file.info().has_key('content-type'):
+	    	response = HttpResponse (file.read(),mimetype=file.info()['content-type'])
+	    else:
+		response = HttpResponse (file.read())
+	    # Add all the headers recieved to the response
+	    headers = file.info().items()
+	    for header in headers:
+		if is_valid_header (string.lower(header[0])):
+		    response[header[0]] = header[1]
+	    return response
+	except Exception, e:
+	    return HttpResponseServerError("<html><head><title>Error</title></head><body>%s</body></html>" % e, mimetype='text/html; charset=UTF-8')
