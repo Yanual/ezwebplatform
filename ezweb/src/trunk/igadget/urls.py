a663fdb2a8c40ea98c0d4240864d1d953ff54193
aarranz <aarranz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-28 09:43:09 +0000

    Ver #14. (commit 580) Modelo de Persistencia
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@600 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/trunk/igadget/urls.py
+++ ezweb/src/trunk/igadget/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('igadget.views',
 
     # IGadgets
     (r'^$', IGadgetCollection(permitted_methods=('GET', 'POST'))),
-    (r'^(?P<vendor>[-ÑñáéíóúÁÉÍÓÚ\w]+)/(?P<name>[-ÑñáéíóúÁÉÍÓÚ\w]+)/(?P<version>[-ÑñáéíóúÁÉÍÓÚ\w]+)/(screen/(?P<screen_id>\d+)/)?$',
+    (r'^(?P<igadget_id>\d+)/$',
         IGadgetEntry(permitted_methods=('GET', 'POST', 'DELETE'))),
 
 )
