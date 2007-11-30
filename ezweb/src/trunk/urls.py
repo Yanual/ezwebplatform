218218754f0bf18927966eaac2d831db124b1a98
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2007-11-30 14:40:15 +0000

    Ver #68. Login and logout uris
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@647 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb/src/trunk/urls.py
+++ ezweb/src/trunk/urls.py
@@ -38,8 +38,8 @@ urlpatterns = patterns('',
     (r'^proxy/', include('proxy.urls')),
    
     # Django contrib
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'^logout/$', 'django.contrib.auth.views.logout'),
-    (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^accounts/login$', 'django.contrib.auth.views.login'),
+    (r'^logout$', 'django.contrib.auth.views.logout'),
+    (r'^admin', include('django.contrib.admin.urls')),
 
 )
