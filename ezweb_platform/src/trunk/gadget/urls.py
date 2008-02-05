6078bcbc057590015975cfd5a0830e256caddcb5
aarranz <aarranz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2008-02-05 15:59:10 +0000

    Ver #73. Fix url.py for gadgets. This allow gadgets with "special" chars (spaces, non-english letters, etc.) in their name and vendor fields.
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@870 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb_platform/src/trunk/gadget/urls.py
+++ ezweb_platform/src/trunk/gadget/urls.py
@@ -46,10 +46,10 @@ urlpatterns = patterns('gadget.views',
 
     # Gadgets
     (r'^$', GadgetCollection(permitted_methods=('GET', 'POST'))),
-    (r'^/(?P<vendor>[-ÑñáéíóúÁÉÍÓÚ\w]+)/(?P<name>[-ÑñáéíóúÁÉÍÓÚ\w]+)/(?P<version>[\._-ÑñáéíóúÁÉÍÓÚ\w]+)$',
+    (r'^/(?P<vendor>[^/\t\n\r\f\v]+)/(?P<name>[^/\t\n\r\f\v]+)/(?P<version>[\._-ÑñáéíóúÁÉÍÓÚ\w]+)$',
         GadgetEntry(permitted_methods=('GET', 'DELETE', 'PUT'))),
-    (r'^/(?P<vendor>[-ÑñáéíóúÁÉÍÓÚ\w]+)/(?P<name>[-ÑñáéíóúÁÉÍÓÚ\w]+)/(?P<version>[\._-ÑñáéíóúÁÉÍÓÚ\w]+)/template$',
+    (r'^/(?P<vendor>[^/\t\n\r\f\v]+)/(?P<name>[^/\t\n\r\f\v]+)/(?P<version>[\._-ÑñáéíóúÁÉÍÓÚ\w]+)/template$',
         GadgetTemplateEntry(permitted_methods=('GET', ))),
-    (r'^/(?P<vendor>[-ÑñáéíóúÁÉÍÓÚ\w]+)/(?P<name>[-ÑñáéíóúÁÉÍÓÚ\w]+)/(?P<version>[\._-ÑñáéíóúÁÉÍÓÚ\w]+)/xhtml$',
+    (r'^/(?P<vendor>[^/\t\n\r\f\v]+)/(?P<name>[^/\t\n\r\f\v]+)/(?P<version>[\._-ÑñáéíóúÁÉÍÓÚ\w]+)/xhtml$',
         GadgetCodeEntry(permitted_methods=('GET', ))),   
 )
