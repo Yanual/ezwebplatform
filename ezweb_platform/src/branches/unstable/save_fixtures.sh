b16bf13321801fb97a9e28688451d05c0bf83382
fabio <fabio@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2008-03-31 09:08:18 +0000

    Ver #113. TabSpace -> WorkSpace
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@1017 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb_platform/src/branches/unstable/save_fixtures.sh
+++ ezweb_platform/src/branches/unstable/save_fixtures.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-./manage.py dumpdata --format=xml --indent=4 connectable auth admin contenttypes sessions sites  > connectable/fixtures/initial_data.xml
-./manage.py dumpdata --format=xml --indent=4 gadget > gadget/fixtures/initial_data.xml
-./manage.py dumpdata --format=xml --indent=4 igadget > igadget/fixtures/initial_data.xml
-./manage.py dumpdata --format=xml --indent=4 resource > resource/fixtures/initial_data.xml
-./manage.py dumpdata --format=xml --indent=4 tag > tag/fixtures/initial_data.xml
-./manage.py dumpdata --format=xml --indent=4 tabspace > tabspace/fixtures/initial_data.xml
+python ./manage.py dumpdata --format=xml --indent=4 connectable auth admin contenttypes sessions sites  > connectable/fixtures/initial_data.xml
+python ./manage.py dumpdata --format=xml --indent=4 gadget > gadget/fixtures/initial_data.xml
+python ./manage.py dumpdata --format=xml --indent=4 igadget > igadget/fixtures/initial_data.xml
+python ./manage.py dumpdata --format=xml --indent=4 resource > resource/fixtures/initial_data.xml
+python ./manage.py dumpdata --format=xml --indent=4 tag > tag/fixtures/initial_data.xml
+python ./manage.py dumpdata --format=xml --indent=4 workspace > workspace/fixtures/initial_data.xml
 
 
 
