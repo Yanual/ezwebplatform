a3581fd529805c42c4f727ecb3e28f4e6e9eb42b
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2008-05-28 14:52:34 +0000

    Ver #113. New fixtures for distributing the platform!
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@1211 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb_platform/src/branches/unstable/save_fixtures.sh
+++ ezweb_platform/src/branches/unstable/save_fixtures.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-python ./manage.py dumpdata --format=xml --indent=4 connectable auth admin contenttypes sessions sites  > connectable/fixtures/initial_data.xml
+python ./manage.py dumpdata --format=xml --indent=4 connectable > connectable/fixtures/initial_data.xml
 python ./manage.py dumpdata --format=xml --indent=4 gadget > gadget/fixtures/initial_data.xml
 python ./manage.py dumpdata --format=xml --indent=4 igadget > igadget/fixtures/initial_data.xml
-python ./manage.py dumpdata --format=xml --indent=4 resource > resource/fixtures/initial_data.xml
-python ./manage.py dumpdata --format=xml --indent=4 tag > tag/fixtures/initial_data.xml
+python ./manage.py dumpdata --format=xml --indent=4 catalogue > catalogue/fixtures/initial_data.xml
 python ./manage.py dumpdata --format=xml --indent=4 workspace > workspace/fixtures/initial_data.xml
-python ./manage.py dumpdata --format=xml --indent=4 context > context/fixtures/initial_data.xml
+python ./manage.py dumpdata --format=xml --indent=4 context auth admin  > context/fixtures/initial_data.xml
 
 
 
