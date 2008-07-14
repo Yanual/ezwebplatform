9add37d845aca22525941246d1cbb4a612ac8608
macvaz <macvaz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2008-07-14 11:11:21 +0000

    refs #223. Returning capabilities info of every gadget!
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@1327 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb_platform/src/branches/digital_home/media/js/dragboard/iGadget.js
+++ ezweb_platform/src/branches/digital_home/media/js/dragboard/iGadget.js
@@ -217,12 +217,17 @@ IGadget.prototype.paint = function(where) {
 	button.setAttribute("class", "closebutton");
 	button.setAttribute("className", "closebutton"); //IE hack
 	
-	var remove_and_cancel = function () { OpManagerFactory.getInstance().removeInstance(this.id); OpManagerFactory.getInstance().unsubscribeServices(this.id); LayoutManagerFactory.getInstance().hideCover();}.bind(this);
-	
-	var remove = function () { OpManagerFactory.getInstance().removeInstance(this.id); LayoutManagerFactory.getInstance().hideCover();}.bind(this);
-	
-	Event.observe (button, "click", function() {LayoutManagerFactory.getInstance().showWindowMenu('cancelService', remove_and_cancel, remove);}, true);
-	
+	if (this.gadget.isContratable()) {
+		var remove_and_cancel = function () { OpManagerFactory.getInstance().removeInstance(this.id); OpManagerFactory.getInstance().unsubscribeServices(this.id); LayoutManagerFactory.getInstance().hideCover();}.bind(this);
+		
+		var remove = function () { OpManagerFactory.getInstance().removeInstance(this.id); LayoutManagerFactory.getInstance().hideCover();}.bind(this);
+		
+		Event.observe (button, "click", function() {LayoutManagerFactory.getInstance().showWindowMenu('cancelService', remove_and_cancel, remove);}, true);
+	}
+	else {
+		Event.observe (button, "click", function() {OpManagerFactory.getInstance().removeInstance(this.id);}.bind(this), true);
+	}
+		
 	button.setAttribute("title", gettext("Close"));
 	button.setAttribute("alt", gettext("Close"));
 	this.gadgetMenu.appendChild(button);
