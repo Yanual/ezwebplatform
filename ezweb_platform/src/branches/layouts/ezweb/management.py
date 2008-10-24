196e6fa22f4b8a27d187227faf9fd8ddd2e85acd
aarranz <aarranz@2a98c5bb-d66c-48ca-a96c-db028a6c31f4>
2008-10-24 11:19:19 +0000

    refs #271. Create a new up to date layout branch (1/2).
    git-svn-id: file:///home/borja/svnDUMPS/ezwebplatform@1476 2a98c5bb-d66c-48ca-a96c-db028a6c31f4

--- ezweb_platform/src/branches/layouts/ezweb/management.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.contrib.auth.management import create_superuser
-from django.contrib.auth import models as auth_app 
-from django.db.models import signals
-
-# Para que no pregunte la creacion de usuario admin,
-# se crea automaticamente
-signals.post_syncdb.disconnect(create_superuser, sender=auth_app)
--- ezweb_platform/src/branches/layouts/ezweb/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns
-
-urlpatterns = patterns('ezweb.views',
-    (r'^$', 'index'),
-    (r'^wiring$', 'wiring'),
-
-)
